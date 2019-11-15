b01ea9b956d94c192e26f7620586a7b54dc4e02f
felipepatente <felipepatente@yahoo.com.br>
2019-11-15 10:19:58 -0300

    Fazendo a inserção na lista de uma nova instituição

--- EnsinoSuperior/EnsinoSuperior/Controllers/InstituicaoController.cs
+++ EnsinoSuperior/EnsinoSuperior/Controllers/InstituicaoController.cs
@@ -54,5 +54,15 @@ namespace EnsinoSuperior.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Instituicao instituicao)
+        {
+            instituicoes.Add(instituicao);
+            instituicao.InstituicaoID = instituicoes.Select(i => i.InstituicaoID).Max() + 1;
+
+            return RedirectToAction("Index");
+        }
     }
 }
