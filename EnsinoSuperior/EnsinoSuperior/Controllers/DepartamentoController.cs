5d4c323213559e42c8449de0848e586d51e60f44
felipepatente <felipepatente@yahoo.com.br>
2019-11-17 11:21:27 -0300

    Criação da estrutura (Controller/View) para a criação de um novo Departamento

---
+++ EnsinoSuperior/EnsinoSuperior/Controllers/DepartamentoController.cs
@@ -0,0 +1,48 @@
+﻿using EnsinoSuperior.Data;
+using EnsinoSuperior.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EnsinoSuperior.Controllers
+{
+    public class DepartamentoController : Controller
+    {
+        private readonly IESContext _context;
+
+        public DepartamentoController(IESContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Departamentos.OrderBy(c => c.Nome).ToListAsync());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Nome")] Departamento departamento)
+        {
+            try
+            {
+                _context.Add(departamento);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch (DbUpdateException)
+            {
+                ModelState.AddModelError("", "Não foi possível inserir dados.");
+            }
+
+            return View(departamento);
+        }
+    }
+}
