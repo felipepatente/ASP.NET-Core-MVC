821dcda412c7dfcbf652fc80e599c2b859e7c5d2
felipepatente <felipepatente@yahoo.com.br>
2019-11-17 09:36:22 -0300

    Criando a estrutura para se conectar a uma base dados

---
+++ EnsinoSuperior/EnsinoSuperior/Data/IESContext.cs
@@ -0,0 +1,26 @@
+﻿using EnsinoSuperior.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EnsinoSuperior.Data
+{
+    public class IESContext: DbContext
+    {
+        public DbSet<Departamento> Departamentos { get; set; }
+        public DbSet<Instituicao> Instituicoes { get; set; }
+
+        public IESContext(DbContextOptions<IESContext> options): base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Departamento>().ToTable("Departamento");
+        }
+    }
+}
