69cf27b281ef0e72bf43440e1d36c376f035700e
felipepatente <felipepatente@yahoo.com.br>
2019-12-07 10:01:00 -0300

    Criando uma classe para fazer a manipulação de dados das instituições

---
+++ EnsinoSuperior/EnsinoSuperior/Data/DAL/Cadastros/InstituicaoDAL.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Modelo.Cadastros;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EnsinoSuperior.Data.DAL.Cadastros
+{
+    public class InstituicaoDAL
+    {
+        private IESContext _context;
+
+        public InstituicaoDAL(IESContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<Instituicao> ObterInstituicoesClassificadasPorNome()
+        {
+            return _context.Instituicoes.OrderBy(b => b.Nome);
+        }
+
+        public async Task<Instituicao> ObterInstituicaoPorId(long id)
+        {
+            return await _context.Instituicoes.Include(d => d.Departamentos)
+                .SingleOrDefaultAsync(m => m.InstituicaoID == id);
+        }
+    }
+}
