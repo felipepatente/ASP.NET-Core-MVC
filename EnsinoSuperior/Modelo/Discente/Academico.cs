8c22604cbf20cde46e52354cf460e9034ee7ae3a
felipepatente <felipepatente@yahoo.com.br>
2019-12-08 18:22:35 -0300

    Criando as visões para trabalhar com a classe academico

--- EnsinoSuperior/Modelo/Discente/Academico.cs
+++ EnsinoSuperior/Modelo/Discente/Academico.cs
@@ -1,15 +1,18 @@
 ﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace Modelo.Discente
 {
     public class Academico
     {
+        [DisplayName("Id")]
         public long? AcademicoID { get; set; }
 
         [StringLength(10, MinimumLength = 10)]
         [RegularExpression("([0-9]{10})")]
         [Required]
+        [DisplayName("RA")]
         public string RegistroAcademico { get; set; }
 
         [Required]
