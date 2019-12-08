ef7a198e32dd717784e87337b561362514bba175
felipepatente <felipepatente@yahoo.com.br>
2019-12-08 17:37:05 -0300

    Fazendo o relacionamento entre as classes

--- EnsinoSuperior/EnsinoSuperior/Data/IESContext.cs
+++ EnsinoSuperior/EnsinoSuperior/Data/IESContext.cs
@@ -13,10 +13,24 @@ namespace EnsinoSuperior.Data
             //IESDbInitializer.Initialize(this);
         }
 
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-        //    base.OnModelCreating(modelBuilder);
-        //    modelBuilder.Entity<Departamento>().ToTable("Departamento");
-        //}
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //modelBuilder.Entity<Departamento>().ToTable("Departamento");
+
+            modelBuilder.Entity<CursoDisciplina>()
+                .HasKey(cd => new { cd.CursoID, cd.DisciplinaID });
+
+            modelBuilder.Entity<CursoDisciplina>()
+                .HasOne(c => c.Curso)
+                .WithMany(cd => cd.CursosDisciplinas)
+                .HasForeignKey(c => c.CursoID);
+
+            modelBuilder.Entity<CursoDisciplina>()
+                .HasOne(d => d.Disciplina)
+                .WithMany(cd => cd.CursosDisciplinas)
+                .HasForeignKey(d => d.DisciplinaID);            
+        }
     }
 }
