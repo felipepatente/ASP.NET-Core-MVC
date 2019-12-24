19a8f5872e867ee561d13587b240d5274d964386
felipepatente <felipepatente@yahoo.com.br>
2019-12-24 10:57:18 -0300

    Atualizando a base de dados (adicionando uma migração)

---
+++ EnsinoSuperior/EnsinoSuperior/Migrations/20191224135428_AdicionandoTabelasProfessorCursoProfessor.cs
@@ -0,0 +1,153 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EnsinoSuperior.Migrations
+{
+    public partial class AdicionandoTabelasProfessorCursoProfessor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cursos_Departamentos_DepartamentoID",
+                table: "Cursos");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Departamentos_Instituicoes_InstituicaoID",
+                table: "Departamentos");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Departamentos",
+                table: "Departamentos");
+
+            migrationBuilder.RenameTable(
+                name: "Departamentos",
+                newName: "Departamento");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Departamentos_InstituicaoID",
+                table: "Departamento",
+                newName: "IX_Departamento_InstituicaoID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Departamento",
+                table: "Departamento",
+                column: "DepartamentoID");
+
+            migrationBuilder.CreateTable(
+                name: "Professor",
+                columns: table => new
+                {
+                    ProfessorID = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Professor", x => x.ProfessorID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CursoProfessor",
+                columns: table => new
+                {
+                    CursoProfessorID = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CursoID = table.Column<long>(nullable: true),
+                    ProfessorID = table.Column<long>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CursoProfessor", x => x.CursoProfessorID);
+                    table.ForeignKey(
+                        name: "FK_CursoProfessor_Cursos_CursoID",
+                        column: x => x.CursoID,
+                        principalTable: "Cursos",
+                        principalColumn: "CursoID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_CursoProfessor_Professor_ProfessorID",
+                        column: x => x.ProfessorID,
+                        principalTable: "Professor",
+                        principalColumn: "ProfessorID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CursoProfessor_CursoID",
+                table: "CursoProfessor",
+                column: "CursoID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CursoProfessor_ProfessorID",
+                table: "CursoProfessor",
+                column: "ProfessorID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cursos_Departamento_DepartamentoID",
+                table: "Cursos",
+                column: "DepartamentoID",
+                principalTable: "Departamento",
+                principalColumn: "DepartamentoID",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Departamento_Instituicoes_InstituicaoID",
+                table: "Departamento",
+                column: "InstituicaoID",
+                principalTable: "Instituicoes",
+                principalColumn: "InstituicaoID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cursos_Departamento_DepartamentoID",
+                table: "Cursos");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Departamento_Instituicoes_InstituicaoID",
+                table: "Departamento");
+
+            migrationBuilder.DropTable(
+                name: "CursoProfessor");
+
+            migrationBuilder.DropTable(
+                name: "Professor");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Departamento",
+                table: "Departamento");
+
+            migrationBuilder.RenameTable(
+                name: "Departamento",
+                newName: "Departamentos");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Departamento_InstituicaoID",
+                table: "Departamentos",
+                newName: "IX_Departamentos_InstituicaoID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Departamentos",
+                table: "Departamentos",
+                column: "DepartamentoID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cursos_Departamentos_DepartamentoID",
+                table: "Cursos",
+                column: "DepartamentoID",
+                principalTable: "Departamentos",
+                principalColumn: "DepartamentoID",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Departamentos_Instituicoes_InstituicaoID",
+                table: "Departamentos",
+                column: "InstituicaoID",
+                principalTable: "Instituicoes",
+                principalColumn: "InstituicaoID",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
--- EnsinoSuperior/Modelo/Discente/CursoProfessor.cs
+++ EnsinoSuperior/Modelo/Discente/CursoProfessor.cs
@@ -4,6 +4,8 @@ namespace Modelo.Discente
 {
     public class CursoProfessor
     {
+        public long CursoProfessorID { get; set; }
+
         public long? CursoID { get; set; }
         public Curso Curso { get; set; }
         public long? ProfessorID { get; set; }
